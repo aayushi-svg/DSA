dd485092fbe462e419d7d1e3600b3a4d1b2b895d
Aayushi Kumari <aayushikumaribth05@gmail.com>
2026-02-04 00:22:27 +0530

    Time: 3 ms (19.94%) | Memory: 8.6 MB (13.31%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -21,4 +21,16 @@ public:
         }
         return ans;
     }
+
+    //   int ans = 1;
+    //   for(int i = 1; i<= x; i++){
+    //      if(i*i <= x){
+    //         ans= i;
+    //     }
+    //     else{
+    //         break;
+    //     }
+    //   }
+    //   return ans;
+    
 };
