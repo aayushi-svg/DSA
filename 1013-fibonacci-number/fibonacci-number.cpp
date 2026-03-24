97e0dec995c12250a934f459820978fcf2e800df
Aayushi Kumari <aayushikumaribth05@gmail.com>
2026-03-24 22:25:38 +0530

    Time: 3 ms (65.33%) | Memory: 8.3 MB (10.40%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
+
+    int fibon(int n,vector<int>&dp) {
+        if (n<=1){
+            return n;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+       return dp[n] = fibon(n - 1, dp) + fibon(n - 2 ,dp);
         
-        return fib(n-1) + fib(n-2);
-       
+    }
+
+    int fib(int n) {
+        
+        vector<int> dp(n+1 ,-1);
+        return fibon(n, dp);
+
     }
 };
